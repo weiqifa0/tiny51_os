4eb2044b326ed7362f2afaed7809aed516c629bb
crisqifawei <329410527@qq.com>
2023-09-29 16:10:22 +0800

    删除不使用的文件

--- delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DELAY_H__
-#define __DELAY_H__
-
-void delay_us(unsigned int nus);
-void delay_ms(unsigned int nms);
-
-#endif
-
--- main.c
+++ main.c
@@ -7,70 +7,141 @@
 #include "machine_hal/machine_config.h"
 #include "task_scheduling_core/task_scheduling_core.h"
 
-/****小小调度器开始**********************************************/
-#define MAXTASKS 2
-static unsigned char timers[MAXTASKS];
-unsigned char currdt;
-#define _SS static unsigned char _lc; switch(_lc) { default:
-#define _EE ;}; _lc=0; return 255;
-#define WaitX(tickets) do {_lc=__LINE__+((__LINE__%256)==0); return tickets ;} while(0); case __LINE__+((__LINE__%256)==0):
-#define RunTask(TaskName,TaskID) do { if (timers[TaskID]==0) timers[TaskID]=TaskName(); } while(0);
+#define uint unsigned int
+#define uchar unsigned char
+#define taskNum 3
+#define taskStackLen 13
 
-#define CallSub(SubTaskName) do { _lc=__LINE__+((__LINE__%256)==0); return 0; case __LINE__+((__LINE__%256)==0): currdt=SubTaskName(); if(currdt!=255) return currdt;} while(0);
+int a;
+#define led0 TINY51_OS_GPIO1_1
+#define led1 TINY51_OS_GPIO1_2
+#define led2 TINY51_OS_GPIO1_3
+#define led3 TINY51_OS_GPIO1_4
 
-#define SEM unsigned int
-//初始化信号量
-#define InitSem(sem) sem=0;
-//等待信号量
-#define WaitSem(sem) do{ sem=1; WaitX(0); if (sem>0) return 1;} while(0);
-//等待信号量或定时器溢出， 定时器tickets 最大为0xFFFE
-#define WaitSemX(sem,tickets) do { sem=tickets+1; WaitX(0); if(sem>1){ sem--; return 1;} } while(0);
-//发送信号量
-#define SendSem(sem) do {sem=0;} while(0);
+uchar flag=0,num=0;
+uchar *pstk[taskNum];
+uchar temp,i;
+uint delayList[3]={0,0,0};
+uchar interruptedList[3]={0,0,0};
 
-int UpdateTimers(void)
+void osDelay(uint delayTime, uchar n){
+  delayList[n]= delayTime*0.903; //这个相当于是乘以一个数，让它稍微小一点。
+  interruptedList[n]=1;
+    while(1){
+      if(interruptedList[n]==0)	{
+        break;
+    }
+  }
+}
+
+void delay(uint z) {
+ static unsigned int x, y;
+ for(x=z; x>0; x--)
+  for(y=110; y>0; y--);  //原来为110
+}
+
+int detect() {	//模拟的任务执行函数。比如……调用超声波。
+	delay(2);
+	led3 = !led3;
+	delay(2);
+	return 0;
+}
+void InitTimer0(void) {
+	TMOD = 0x01;
+  TH0 = 0x0FC;
+  TL0 = 0x18;
+  EA = 1;
+  ET0 = 1;
+  TR0 = 1;
+}
+
+
+void taskA() {
+ while(1) {
+  temp=0x01;
+  led0 = !led0;
+  osDelay(10,0);
+ }
+}
+void taskB() {
+static int b;
+ while(1) {
+  led1 = !led1;
+  b=detect();
+  osDelay(1000,1);
+ }
+}
+
+void taskC() {
+ while(1) {
+  delay(20);
+ }
+}
+
+void main(void) {
+ InitTimer0();
+ while(1){
+
+ };
+}
+void * const func[]={taskA,taskB,taskC};
+
+void exint0() __interrupt (1)
 {
-  unsigned char i;
-  for(i = MAXTASKS; i > 0; i--){
-    if((timers[i - 1] != 0) && (timers[i - 1] != 255))
-      timers[i - 1]--;
-  }
-  return 0;
-}
 
-void INTT0(void) __interrupt (1) __using (1)
-{
-  UpdateTimers();
-  TL0 = 0Xff;//10ms 重装
-  TH0 = 0XDB;//b7;   
-}
+  uchar __idata *p;
+  uchar q;
 
-int task1(void) {
-_SS
-  while(1) {
-    WaitX(50);
-    LED1 = !LED1;
-  }
-_EE
-}
+  led2 = !led2;
 
-int task2(void) {
-_SS
-  while(1) {
-    WaitX(100);
-    LED2=!LED2;
-  }
-_EE
-}
+  if(num==1) pstk[0]=(uchar *)SP;//num=1表示第二次进入中断（主函数中没有任务运行），在进入任务后产生的中断，这时保存该任务的进入中断前的堆栈指针
+  if((num==2)&&(num==taskNum-1)) pstk[1]=(uchar *)SP;	 //第三次进入中断的时候需要
 
-void main(void)
-{
-  tiny51_os_timer_init();
-  //set_timer_irq_callback_function(&UpdateTimers);
-  while(1) {
-    RunTask(task1, 0);
-    RunTask(task2, 1);
+
+  //执行这一步！但是为什么呢？
+  if(num<taskNum)		   //这种情况有几个任务就在初次启动时启动几次。
+  {
+    p = (uchar __idata *)SP+1;	//将SP强制类型转换为UCHAR类型的指针，并且在SP的地址的基础上加1。
+    *p++=((uint)(func[num]))%256;
+    *p=((uint)(func[num]))/256;   //将右边赋给p指针所指向的地址。这里指的是，取出func[num]这个函数的首地址，除以256之后再赋给p指向的地址。
+    SP=(uchar)p;          	    //	强制类型转换，让SP指向p的地址。
+    SP+=10;	                   // 我不懂这句是啥意思……
+    num++;	                	//当前的任务号:自增。
+                                //这一部分应该能自动增加。
   }
+  else
+  {
+    static unsigned char lastFlag=2;
+
+    flag=2;
+    q=flag;
+    if(delayList[0]>0) {
+      delayList[0]-=1;
+    }else{
+      q=flag;
+      flag=0;
+    }
+
+    if(delayList[1]>0){
+      delayList[1]-=1;
+    }else{
+      q=flag;
+      flag=1;
+    }
+    interruptedList[0]=0;
+    interruptedList[1]=0;
+    interruptedList[2]=0;
+
+
+    pstk[lastFlag]=	 (uchar *)SP;
+    lastFlag = flag;
+    SP = (uchar)pstk[flag];
+  }
+  TH0 = 0x0FC;
+  TL0 = 0x18;
 }
+//此程序是通过定时器中断来切换的,但是切换时对堆栈的使用上存在问题。
+//该程序的目的不是展示完美的任务级切换,主要是如何切换的问题。
+
 
 
